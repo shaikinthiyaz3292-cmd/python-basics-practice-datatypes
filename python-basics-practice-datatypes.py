2b813c49aca246a005b7e2468df03710fc466459
shaikinthiyaz3292-cmd <shaikinthiyaz3292@gmail.com>
2026-02-18 13:38:41 +0530

    Add files via upload

---
+++ python-basics-practice-datatypes.py
@@ -0,0 +1,116 @@
+# name = "inthiyaz"
+# print(name)
+# age = 21
+# print("my age is", age)
+
+# a = 10
+# b = 20
+# print(a+b)
+
+# price = 13.5
+# print(type(price))
+
+# name = "inthiyaz"
+# print(name[0])
+
+# name = "inthu"
+# age= 21
+# height = 6.0
+# print("my name is" , name, "i am", age, "years old and my height is" ,height )
+
+# num = 8
+# num_float = float(num)
+
+# print("Integer value:", num)
+# print("Float value:", num_float)
+
+# num = 7
+# price = 3.5
+# place = "pml"
+
+# print(type(num))
+# print(type(price))
+# print(type(place))
+
+# a = 7
+# b = 9
+
+# a, b = b, a
+
+# print("a = ", a)
+# print("b =", b)
+
+# name = "intu"
+# print(type(name))
+
+# a = int(input("enter first num: "))
+# b = int(input("enter second num: "))
+        
+# print("sum = ",a+b)
+# print("difference = ",a-b)
+# print("multiplication = ",a*b)
+
+# num = int(input("enter the number: "))
+# print("num =" , num + 10)
+
+
+# list = ["sirai", "valimai","love"]
+# print(list[1])
+
+# is_nay = True
+# is_sou = False
+# print(type(is_nay))
+
+
+# name = "inthiyaz"
+# print(len(name))
+
+
+# name = (input("enter your name: "))
+# age = int(input("enter your age: "))
+
+# print(f"hello {name}, next year you will be {age+1}")
+
+# num = int(input("enter a number: "))
+
+# if num % 2 == 0:
+#     print("even number")
+# else:
+#     print("odd number")
+
+# celsius = float(input("enter the tempertature in celsius: "))
+# fahrenheit = (celsius * 9/5) + 32
+# print("temperature in fahreheit =", fahrenheit)
+
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+
+# if a > b:
+#     print("First number is greater")
+# elif b > a:
+#     print("Second number is greater")
+# else:
+#     print("Both numbers are equal")
+
+
+# num = int(input("Enter a number: "))
+# print("Square =", num * num)
+
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+# c = int(input("Enter third number: "))
+
+# print("Largest =", max(a, b, c))
+
+
+# num = int(input("Enter a number: "))
+
+# if num > 0:
+#     print("Positive")
+# elif num < 0:
+#     print("Negative")
+# else:
+#     print("Zero")
+
+# text = input("Enter text: ")
+# print(text.upper())
